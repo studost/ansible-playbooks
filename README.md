ff323ae320c2bbb7c6d38220e37959ec27d7bd95
Udo Stachowiak <ustachowiak@freicon.de>
2021-03-17 17:04:08 +0100

    Changes

--- README.md
+++ README.md
@@ -5,4 +5,16 @@ GitHub Repo for my ansible playbooks
 mkdir ansible && cd ansible
 git clone git@github.com:studost/ansible-playbooks.git
 
+## Test 2021_03_17
+
+Ansible-Host fcolitsmtest01 / 172.25.221.65 / User itsm
+Ansible-Client ans01 / 172.16.160.129 / 172.16.10.128 / User ansible
+
+
+### Examples / Tasks for playbooks
+
+Install packages:
+* net-tools
+
+
 
--- scripts/debansstrap.sh
+++ scripts/debansstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # enable host for ansible
 set -e
-PUBKEY=""
+PUBKEY="ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAEKiJyguZ21oodAe/i9EcvU5zv3eKX1LnmtcyEa1yFZ8DTokA9UTEPHxpcmOqJ04N5tPt3IRTsHRSTB83bMqMrvCQFxIUH+ybZp4LwL6XHw12mPranWDuFez1FMLZrVz8N+Z2H+7BSVySGGEUojo1pVBXMGm0cejdDj5v//mgYLGjosSQ== studo@USTACHOWIAKNB02"
 id -u ansible > /dev/null 2>&1 || \
 adduser ansible --disabled-password \
 --gecos "" --quiet
